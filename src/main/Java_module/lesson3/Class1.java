5d5d9d4246f69f41eb21fadb660c767e1d6736dc
stranik912 <Stranik912@yandex.ru>
2022-08-08 22:03:53 +0300

    lsdasldlas

---
+++ src/main/Java_module/lesson3/Class1.java
@@ -0,0 +1,7 @@
+package main.Java_module.lesson3;
+
+public class Class1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/Java_module/lesson3/Class2.java
@@ -0,0 +1,7 @@
+package main.Java_module.lesson3;
+
+public class Class2 {
+    public static void main(String[] args) {
+
+    }
+}
