1e0bb85df4cd85e7f8e50b39e1a2ddf9b479fbe3
stranik912 <Stranik912@yandex.ru>
2022-08-15 21:39:16 +0300

    lsdasldlas

--- src/main/Java_module/lesson2/myClass.java
+++ src/main/Java_module/lesson2/myClass.java
@@ -2,4 +2,6 @@ public class myClass {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
 }
+
--- src/main/Java_module/lesson3/HomeWorkPart2.java
+++ src/main/Java_module/lesson3/HomeWorkPart2.java
@@ -54,4 +54,5 @@ public class HomeWorkPart2 {
             System.out.print( result + " ");
             counter++;}
     }
+
 }
